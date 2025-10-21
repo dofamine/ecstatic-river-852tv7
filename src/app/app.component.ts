c5cc3034f8bb13b6d2c47b7a913dc9f696e6c36f
dofamine <pryatko.oleg@gmail.com>
2025-10-21 14:10:00 +0200

    updated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,20 @@ import { Component, Input } from '@angular/core';
   selector: 'app-table',
   standalone: true,
   imports: [NgStyle],
-  providers: [HttpClient],
   template: `
     <h1>{{ tableName }}</h1>
     <table>
       <tr>
         @for (col of colDef; track col) {
-        <td
-          [ngStyle]="{
-            width: col.width + 'px',
-            visibility: col.hidden ? 'hidden' : 'visible'
-          }"
-        >
-          {{ col.name }}
-        </td>
+          <td [ngStyle]="{ width: col.width + 'px', visibility: col.hidden ? 'hidden' : 'visible' }"> {{ col.name }} </td>
         }
       </tr>
       @for (record of data; track record) {
-      <tr>
-        @for (key of getDataKeys(record); track key; let index = $index) {
-        <td
-          [ngStyle]="{
-            visibility: colDef[index].hidden ? 'hidden' : 'visible'
-          }"
-          (dblclick)="getStatus(record)"
-        >
-          {{ record[key] }}
-        </td>
-        }
-      </tr>
+        <tr>
+          @for (key of getDataKeys(record); track key; let index = $index) {
+            <td [ngStyle]="{visibility: colDef[index].hidden ? 'hidden' : 'visible'}" (dblclick)="getStatus(record)">{{ record[key] }}</td>
+          }
+        </tr>
       }
     </table>
   `,
@@ -51,31 +36,20 @@ export class TableComponent {
   }
 
   getStatus(record: any): void {
-    this.http
-      .get<any[]>('assets/orders.json')
-      .subscribe((res) => alert(res.find(({ id }) => id === record.id).status));
+    this.http.get<any[]>('assets/orders.json').subscribe((res) => alert(res.find(({ id }) => id === record.id).status));
   }
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<app-table
-    tableName="My Table"
-    [colDef]="colDef"
-    [data]="data"
-  ></app-table>`,
+  template: `<app-table tableName="Orders" [colDef]="colDef" [data]="data" />`,
   imports: [TableComponent],
   providers: [HttpClient],
 })
 export class AppComponent {
   data: any;
-  colDef = [
-    { name: 'Order Id', width: 100 },
-    { name: 'Name', width: 200 },
-    { name: 'Status', hidden: true },
-    { name: 'Info', width: 200 },
-  ];
+  colDef = [{ name: 'Order Id', width: 100 }, { name: 'Name', width: 200 }, { name: 'Status', hidden: true }, { name: 'Info', width: 200 }];
 
   constructor(http: HttpClient) {
     http.get('assets/orders.json').subscribe((res) => (this.data = res));
